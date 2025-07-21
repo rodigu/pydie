350714f083e91014ac0c6fa6b3eabb3f7c58c4e1
rodigu <rmoraisu@protonmail.com>
2025-07-21 19:36:00 -0300

    #6 - intial rewrite setup

--- src/pydie/rest/fetchers.py
+++ src/pydie/rest/fetchers.py
@@ -1,4 +1,53 @@
-import interfaces as rest
+import pydie.rest.interfaces as rest
+from requests import Response
+from pydie.utils import get_nested_value, parse_path_parameters, inject_parameter_values
+
+
+def fetcher(
+    configuration: rest.FetcherConfiguration,
+    engine_configuration: rest.EngineConfiguration,
+) -> rest.ConverterConfiguration:
+    request_function = rest.REQUEST_FUNCTIONS[configuration["request_function_name"]]
+    response: Response = request_function(
+        url=configuration["id"], **configuration["request_function_parameters"]
+    )
+    if response.status_code != 200:
+        raise BaseException(
+            f"Failed request to [{configuration['id']}] (status {response.status_code}).\n"
+            + f"Details: {response.reason}."
+        )
+    data = response.json()
+    dependent_requests = extract_parametrizable_properties_into_dependent_requests(
+        data=data,
+        parametrizable_properties=configuration["response_parametrizable_properties"],
+    )
+
+    return {
+        "dependent_requests_parameters": dependent_requests,
+        "target_table": configuration["target_table"],
+    }
+
+
+def extract_parametrizable_properties_into_dependent_requests(
+    data: dict, parametrizable_properties: rest.ParametrizablePropertyMapping
+) -> rest.DependentRequestMapping:
+    dependent_requests: rest.DependentRequestMapping = dict()
+    for parameter_key, property in parametrizable_properties.items():
+        fetcher_id = property["fetcher_id"]
+        if fetcher_id not in dependent_requests:
+            dependent_requests[fetcher_id] = {"id": fetcher_id, "parameters": dict()}
+        data_at_address = get_data_at_address(data=data, address=property["address"])
+        dependent_requests[fetcher_id]["parameters"].update(
+            {
+                property["parameter_key"]: data_at_address
+                if isinstance(data_at_address, list)
+                else [data_at_address]
+            }
+        )
+        dependent_fetcher_id = inject_parameter_values(path=fetcher_id)
+        property["fetcher_id"]
+        property["parameter_key"]
+    # TODO, set target table to raw (unparametrized) FetcherID
 
 
 def path_maker(
@@ -34,14 +83,36 @@ def dependency_injector(
     :param rest.ResponseData response_data: response data from parent request
     :return dict[rest.Path, rest.FetcherConfiguration]:
     """
+    for path, property in response_properties.items():
+        data: list[dict] = get_data_at_address(
+            data=response_data, address=property["parametrizable_proterty_address"]
+        )
+        key = property["parametrizable_key"]
+        parameter_name = property["path_parameter_name"]
+
+        if "target_table" not in dependent_requests[path]:
+            dependent_requests[path]["target_table"] = path
+        if dependent_requests[path]["parametrizable_values"] is None:
+            dependent_requests[path]["parametrizable_values"] = dict()
+        dependent_requests[path]["parametrizable_values"].update(
+            {parameter_name: [v[key] for v in data]}
+        )
+    return dependent_requests
 
 
-def fetcher_to_fetcher_adaptor(
+def dependency_extractor(
     converter_configuration: rest.ConverterConfiguration,
-) -> list[rest.FetcherConfiguration]: ...
+) -> list[rest.FetcherConfiguration]:
+    """Splits dependent requests in converter configuration into separate fetcher configurations
+
+    :param rest.ConverterConfiguration converter_configuration: converter configuration output from a fetcher
+    :return list[rest.FetcherConfiguration]:
+    """
 
 
-def get_data_at_address(data: dict, address: rest.ResponsePropertyKey):
+def get_data_at_address(data: dict | list, address: rest.ResponsePropertyKey):
+    if address is None or type(data) is list:
+        return data
     index = 0
     result = data
     while index < len(address):
@@ -66,16 +137,16 @@ def fetcher(
         path_parameters=connection.get("parametrizable_values"),
     )
 
-    connection["request_function_parameters"]["headers"].update(
+    connection.get("request_function_parameters", {}).get("headers", {}).update(
         engine["default_headers"]
     )
 
     response = connection["request_function"](
-        url=url, **connection["request_function_parameters"]
+        url=url, **connection.get("request_function_parameters", {})
     )
 
     if response.status_code != 200:
-        raise BaseException(response, response.reason)
+        raise BaseException(url, response, response.reason)
 
     response_data = response.json()
 
@@ -91,7 +162,12 @@ def fetcher(
     )
 
     return {
-        "destination_table": connection["destination_table"],
+        "target_table": connection["target_table"],
         "data": response_data,
         "dependent_requests": dependencies,
     }
+
+
+def dependent_fetcher(
+    connection: rest.FetcherConfiguration, engine: rest.EngineConfiguration
+) -> rest.ConverterConfiguration: ...
